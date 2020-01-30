8bb3a764009dc4f970092950a5337dd784459023
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2020-01-30 17:55:04 +0700

    - Fixed issue related to import EntryConverter - Removed signing.gradle signingConfig

--- app/src/main/java/co/kyald/coronavirustracking/data/database/AppDatabase.kt
+++ app/src/main/java/co/kyald/coronavirustracking/data/database/AppDatabase.kt
@@ -3,9 +3,9 @@ package co.kyald.coronavirustracking.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import co.kyald.coronavirustracking.data.database.converter.EntryConverter
 import co.kyald.coronavirustracking.data.model.CoronaEntity
 import co.kyald.coronavirustracking.data.model.DummyEntity
-import co.kyald.coronavirustracking.data.model.EntryConverter
 import co.kyald.coronavirustracking.data.database.dao.CoronaDao
 import co.kyald.coronavirustracking.data.database.dao.DummyDao
 
