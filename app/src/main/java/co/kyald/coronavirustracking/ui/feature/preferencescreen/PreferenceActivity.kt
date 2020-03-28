644b839cd457db6b3882801379e7c50ca3d27564
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2020-03-28 19:10:16 +0700

    Removed Channel new asia data source

--- app/src/main/java/co/kyald/coronavirustracking/ui/feature/preferencescreen/PreferenceActivity.kt
+++ app/src/main/java/co/kyald/coronavirustracking/ui/feature/preferencescreen/PreferenceActivity.kt
@@ -23,7 +23,6 @@ import java.util.concurrent.TimeUnit
 
 class PreferenceActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
